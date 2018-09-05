ac17ffdb51f01f660639e45dc322700e2714d1c3
js5323 <js5323@hotmail.com>
2018-09-05 15:53:40 +0800

    将ui相关依赖分离

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -9,6 +9,13 @@ import { NavBar, Tabbar, TabbarItem, Button } from 'vant'
 
 [NavBar, Tabbar, TabbarItem, Button].forEach(item => Vue.use(item))
 
+// 引入基础样式
+import 'vant/packages/vant-css/src/base.css'
+
+// 引入组件对应的样式
+import 'vant/packages/vant-css/src/button.css'
+import 'vant/packages/vant-css/src/checkbox.css'
+
 // import Vant from 'vant';
 // import 'vant/lib/vant-css/index.css';
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,7 +35,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     .home {
         width: 375px;
         height: 100px;
