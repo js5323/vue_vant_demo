c210874eb608452778a58223b2b38be0af0eaa02
js5323 <js5323@hotmail.com>
2018-09-06 10:57:22 +0800

    px2rem when production

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
+
 module.exports = {
   plugins: {
     autoprefixer: {
       browsers: ['last 5 versions']
-    },
-    'postcss-px2rem': {
-      remUnit: 37.5
     }
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins['postcss-px2rem'] = {
+    remUnit: 37.5
   }
 }
